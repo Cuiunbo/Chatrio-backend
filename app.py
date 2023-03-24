499cf91f5e3485cacf6ae20690656cc2b555b127
OzzyHelium <ozzyboogie@outlook.com>
2023-03-24 16:47:15 +0800

    rename

--- app.py
+++ app.py
@@ -1,36 +1,51 @@
 from flask import Flask, request, jsonify
-import mysql.connector
+from flask_cors import CORS
+from utils import Mysql
+from models import User
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    # 获取前端发送过来的用户名和密码
     username = request.json.get('username')
     password = request.json.get('password')
+    # print(username)
+    mysql = Mysql()
+    sql = "select * from users where email = '" + username + "'"
+    # print(sql)
+    a = mysql.fetch_one_db(sql)
+    # print(a)
+    if a is not None:
+        r = User(a)
+        if r.password == password:
+            return jsonify({'success': True}), 200
+    return jsonify({'success': False, 'message': 'Invalid username or password'}), 401
 
-    # 连接数据库
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='password',
-        database='mydatabase'
-    )
 
-    # 执行 SQL 查询
-    cursor = conn.cursor()
-    query = "SELECT * FROM users WHERE username = %s AND password = %s"
-    cursor.execute(query, (username, password))
-    user = cursor.fetchone()
+@app.route('/api/signup', methods=['POST'])
+def signup():
+    flag = False
+    username = request.json.get('username')
+    password = request.json.get('password')
+    email = request.json.get('email')
+    mysql = Mysql()
+    sql1 = "select * from users where email = '" + email + "'"
+    sql2 = "select * from users where user_name = '" + username + "'"
+    r1 = mysql.fetch_one_db(sql1)
+    r2 = mysql.fetch_one_db(sql2)
 
-    # 关闭数据库连接
-    cursor.close()
-    conn.close()
+    print(r1)
+    print(r2)
+    if r1 is not None or r2 is not None:
+        return jsonify({'success': False, 'message': 'Name or email has been occupied!'}), 401
+    sql3 = "insert into chatrio.users (user_name, password, email) values ('" + username + "','" + password + "','" + email + "')"
+    print(sql3)
+    r3 = mysql.exe_db(sql3)
+    print(r3)
+    return jsonify({'success': True}), 200
 
-    # 判断是否登录成功
-    if user:
-        # 如果登录成功，返回登录成功的信息
-        return jsonify({'message': '登录成功'})
-    else:
-        # 如果登录失败，返回登录失败的信息
-        return jsonify({'message': '用户名或密码错误'})
+
+if __name__ == '__main__':
+    app.run()
