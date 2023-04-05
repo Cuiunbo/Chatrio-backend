92bbb6777087ff99009228e67fcf835ed631f627
OzzyHelium <117267845+OzzyHelium@users.noreply.github.com>
2023-04-05 15:16:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask_socketio import SocketIO, send, join_room, leave_room,emit
+from flask_socketio import SocketIO, send, join_room, leave_room, emit
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity, decode_token
 from flask import Flask, request, jsonify
 from flask_cors import CORS
@@ -6,13 +6,13 @@ from utils import Mysql
 from models import User
 
 app = Flask(__name__)
-CORS(app) # 跨域
+CORS(app)  # 跨域
 app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['JWT_SECRET_KEY'] = 'mysecretkey'
 app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 
 jwt = JWTManager(app)
-socketio = SocketIO(app, cors_allowed_origins='*') # 允许所有源
+socketio = SocketIO(app, cors_allowed_origins='*')  # 允许所有源
 
 
 @app.route('/api/login', methods=['POST'])
@@ -34,7 +34,8 @@ def login():
             # response = jsonify({'success': True, 'token': token})
             # print(response.json)
             # print(response.json.get('token'))
-            return jsonify({'success': True, 'token': token, 'username':r.user_name, 'email':r.email}), 200
+            return jsonify(
+                {'success': True, 'token': token, 'username': r.user_name, 'email': r.email, 'userid': r.user_id}), 200
     return jsonify({'success': False, 'message': 'Invalid username or password'}), 401
 
 
@@ -60,37 +61,41 @@ def signup():
     print(r3)
     return jsonify({'success': True}), 200
 
+
 # 消息发送
 @socketio.on('message')
 def handleMessage(msg):
     print(f'Received message: {msg}')
-    mysql = Mysql()
-    #TODO:roomid并没有获取
-    # 插入消息记录到数据库中
-    sql = "INSERT INTO chatrio.messages (room_id, content, user_id,time) VALUES ('{}', '{}', '{}',)".format(msg['roomId'], msg['content']['content'], msg['token'])
-    r3 = mysql.exe_db(sql)
-    print(r3)
+
     send(msg, broadcast=True)
+
 
 # 获取聊天室列表
 @socketio.on('get_room_list')
 def handle_get_room_list(data):
-    # print(data)
+    print(data)
     # decode = decode_token(data)
     user_id = data
     print(user_id)
     mysql = Mysql()
-    sql = "select user_name, user_id from users where user_id != '" + str(user_id) + "'"
+    sql = "select room_id, room_name,num_members from rooms where room_id=any(select room_id from room_user where user_id = " + user_id + ")"
+
     print(sql)
     a = mysql.fetch_all_db(sql)
-    # 再查找 room_user表, 找到所有当前user_id所在的room_id
-    # 查找 room表, 找到所有room_id对应的room_name和room_id
     print(a)
     emit('room_list', a)
 
-#TODO: 当点击某个聊天室时, 如果前端没有该聊天室的
 
+# @socketio.on('fetch_chat_rooms')
+# def handle_fetch_chat_rooms(data):
+#     user_id = data['userId']
 
+#     # Fetch chat rooms for the given user_id
+#     # You might need to adjust the following line according to your database
+#     chat_rooms = get_chat_rooms(user_id)
+
+#     # Emit an event with the chat rooms
+#     emit('chat_rooms', chat_rooms)
 
 
 if __name__ == '__main__':
